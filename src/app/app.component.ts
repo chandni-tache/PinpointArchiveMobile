8f33b878f3fedb02b34670ed22adb11d12be7379
Chandni Garg <chandni@tache.in>
2022-08-18 19:43:52 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,7 +55,7 @@ export class AppComponent {
       w.FCM.onNotification((payload) => {
 
 
-        alert(payload.title + ' ' + payload.body + "AWS notification")
+        alert(payload.title + '-' + payload.body )
 
       })
 
